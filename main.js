a02b98c8b0d9a8537b21ea0f89494c68fd9e5ef0
Marcos Vinicius <marcao.adsistema@gmail.com>
2021-08-19 16:18:13 -0300

    Add Rodapé | Add Voltar para cima

--- main.js
+++ main.js
@@ -15,16 +15,17 @@ for( const link of links ) {
     })
 }
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
     if (window.scrollY >= navHeight) {
         header.classList.add('scroll')
     } else {
         header.classList.remove('scroll')
     }
-})
+}
+
 
 const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
@@ -35,3 +36,36 @@ const swiper = new Swiper('.swiper-container', {
     keyboard: true,
 });
 
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .text, #home .image,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links,
+    footer .brand, footer .social    
+    `,
+    { interval: 100 }
+    )
+
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()        
+})
